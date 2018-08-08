b0f4aa5458c20cda2c611631681cfc8dff01d3ce
diegitsen <diegitsen@gmail.com>
2018-08-08 11:55:59 -0500

    recording is ok

--- app/src/main/java/com/example/diego/mic/fragments/RecordFragment.java
+++ app/src/main/java/com/example/diego/mic/fragments/RecordFragment.java
@@ -1,17 +1,25 @@
 package com.example.diego.mic.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.diego.mic.R;
+import com.example.diego.mic.RecordingService;
+
+import java.io.File;
 
 
 /**
@@ -49,17 +57,19 @@ public class RecordFragment extends Fragment {
     public RecordFragment() {
         // Required empty public constructor
     }
-
+/*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         position = getArguments().getInt(ARG_POSITION);
-    }
+    }*/
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         View recordView = inflater.inflate(R.layout.fragment_record, container, false);
 
         mChronometer = (Chronometer) recordView.findViewById(R.id.chronometer);
@@ -82,6 +92,62 @@ public class RecordFragment extends Fragment {
 
     private void onRecord(boolean start)
     {
+        Intent intent = new Intent(getActivity(), RecordingService.class);
+
+
+        if (start) {
+            // start recording
+            //mRecordButton.setImageResource(R.drawable.ic_media_stop);
+            //mPauseButton.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(),R.string.toast_recording_start,Toast.LENGTH_SHORT).show();
+            File folder = new File(Environment.getExternalStorageDirectory() + "/SoundRecorder");
+            if (!folder.exists()) {
+                //folder /SoundRecorder doesn't exist, create the folder
+                folder.mkdir();
+            }
+
+            //start Chronometer
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            mChronometer.start();
+            //this is not gonna work cause mRecordingPrompt, i created that, but i did not initialized
+            /*
+            mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+                @Override
+                public void onChronometerTick(Chronometer chronometer) {
+                    if (mRecordPromptCount == 0) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+                    } else if (mRecordPromptCount == 1) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "..");
+                    } else if (mRecordPromptCount == 2) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "...");
+                        mRecordPromptCount = -1;
+                    }
+
+                    mRecordPromptCount++;
+                }
+            });*/
+
+            //start RecordingService
+            getActivity().startService(intent);
+            //keep screen on while recording
+            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            //mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+            mRecordPromptCount++;
+
+        } else {
+            //stop recording
+            mRecordButton.setImageResource(R.drawable.ic_mic_white_36dp);
+            //mPauseButton.setVisibility(View.GONE);
+            mChronometer.stop();
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            timeWhenPaused = 0;
+            mRecordingPrompt.setText(getString(R.string.record_prompt));
+
+            getActivity().stopService(intent);
+            //allow the screen to turn off again once recording is finished
+            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
 
     }
 
